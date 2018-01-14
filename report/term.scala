d807e39d3e8568e09990737974f7756cb98157db
Romain Ruetschi <romain.ruetschi@gmail.com>
2018-01-14 02:03:39 +0100

    WIP

--- report/term.scala
+++ report/term.scala
@@ -4,12 +4,20 @@ import stainless.collection._
 
 object term {
 
-  def nonterminating(xs: List[BigInt]): List[BigInt] = {
-    if (xs.isEmpty) Nil() else nonterminating(Cons(xs.head, xs))
+  def f(xs: List[BigInt]): List[BigInt] = {
+    if (xs.isEmpty) Nil() else f(Cons(xs.head, xs))
   }
 
-  def infinite = {
-    nonterminating(List(1)) == Nil[BigInt]()
+  def g(xs: List[BigInt]): List[BigInt] = {
+    g(xs)
+  }
+
+  def bad1 = {
+    f(List(1)) == Nil[BigInt]()
+  } holds
+
+  def ok1 = {
+    g(List(1)) == Nil[BigInt]()
   } holds
 
 }
